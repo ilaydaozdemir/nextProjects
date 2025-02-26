45556b73a4afc0b0c81c1cafdfd89f69a4fae1b0
ozdemirilayda <ozdemirilayda95@gmail.com>
2025-02-26 13:04:44 +0300

    Add component

---
+++ blog/src/components/navbar/links/Links.jsx
@@ -0,0 +1,33 @@
+const { default: Link } = require("next/link");
+
+const Links = () => {
+  const links = [
+    {
+      title: "Homepage",
+      path: "/",
+    },
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+    {
+      title: "Blog",
+      path: "/blog",
+    },
+  ];
+
+  return (
+    <div>
+      {links.map((link) => (
+        <Link href={link.path} key={link.title}>
+          {link.title}
+        </Link>
+      ))}
+    </div>
+  );
+};
+export default Links;
