72e2e5fdb0370489d28b846cc1134e0e0604c0fb
ozdemirilayda <ozdemirilayda95@gmail.com>
2025-03-03 18:32:30 +0300

    Add content and design for About page

--- blog/src/app/about/page.jsx
+++ blog/src/app/about/page.jsx
@@ -1,4 +1,39 @@
+import Image from "next/image";
+import styles from "./about.module.css";
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subTitle}>About Agency</h2>
+        <h1 className={styles.title}>
+          We create digital ideas that are bigger, bolder, breaver and better
+        </h1>
+        <p className={styles.desc}>
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here', making it
+          look like readable English.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/about.png" alt="About Image" fill className={styles.img} />
+      </div>
+    </div>
+  );
 };
 export default AboutPage;
