14231597e69bfeb9fb1ac009c198e57678d6127f
ozdemirilayda <ozdemirilayda95@gmail.com>
2025-03-03 15:44:59 +0300

    Add footer content and styles

--- blog/src/components/footer/Footer.jsx
+++ blog/src/components/footer/Footer.jsx
@@ -1,4 +1,10 @@
+import styles from "./footer.module.css";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>ilaydaozdemir</div>
+      <div className={styles.text}>@All rights reserved.</div>
+    </div>
+  );
 };
 export default Footer;
