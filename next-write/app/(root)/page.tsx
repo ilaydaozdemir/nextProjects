56ec343ca799eacc73ce8f94b43247799c0291c3
ozdemirilayda <ozdemirilayda95@gmail.com>
2025-06-19 21:51:56 +0300

    Add packages

--- next-write/app/(root)/page.tsx
+++ next-write/app/(root)/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <h1 className="uppercase bg-black px-6 py-3 font-work-sans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5">
+        Home
+      </h1>
+      <Button>merhaba</Button>
     </>
   );
 }
